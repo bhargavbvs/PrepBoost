806243558f2e3eb18679d92698e2f64f118dcebd
anudeepreddy36 <37535240+anudeepreddy36@users.noreply.github.com>
2022-02-02 18:57:19 -0500

    Update App.js

--- frontend/prepboost/src/App.js
+++ frontend/prepboost/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is sample one
         </p>
         <a
           className="App-link"
