0a07680191442cd9115bf076138c88a02b60a433
Anudeep Reddy <bandaanudeep36@gmail.com>
2022-02-03 18:41:11 -0500

    Topic wise questions

--- frontend/prepboost/src/components/TopicCards.js
+++ frontend/prepboost/src/components/TopicCards.js
@@ -5,22 +5,22 @@ import CardItem from './CardItem';
 function TopicCards() {
   return (
     <div className='cards'>
-      <h1>Check out the questions of different year here</h1>
+      <h1>Check out the questions of different topics here</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img_2020.png'
+              src='images/eco_min.png'
               text='Economy'
               path='/years'
             />
             <CardItem
-              src='images/img_2020.png'
+              src='images/agri_min.png'
               text='Agriculture'
               path='/years'
             />
             <CardItem
-              src='images/img_2019.png'
+              src='images/env_min.png'
               text='Environment'
               path='/years'
             />
@@ -28,45 +28,90 @@ function TopicCards() {
           
           <ul className='cards__items'>
             <CardItem
-              src='images/img_2018.png'
+              src='images/pol_min.png'
               text='Pol and Gov'
               path='/topics'
             />
             <CardItem
-              src='images/img_2017.png'
+              src='images/ir_min.png'
               text='IR'
               path='/sign-up'
             />
             <CardItem
-              src='images/img_2016.png'
+              src='images/sci_min.png'
               text='S and T'
               path='/sign-up'
-            /><CardItem
-              src='images/img_2015.png'
+            />
+            </ul>
+            
+            <ul className='cards__items'>
+            <CardItem
+              src='images/mod_min.png'
               text='Modern'
+              path='/topics'
+            />
+            <CardItem
+              src='images/anc_min.png'
+              text='Ancient'
+              path='/topics'
+            />
+            <CardItem
+              src='images/med_min.png'
+              text='Medieval'
               path='/topics'
             />
             </ul>
             
             <ul className='cards__items'>
             <CardItem
-              src='images/img_2014.png'
-              text='Ancient'
-              path='/topics'
-            />
-            <CardItem
-              src='images/img_2013.png'
-              text='Medieval'
-              path='/topics'
-            />
-            <CardItem
-              src='images/img_2012.png'
+              src='images/cul_min.png'
               text='Culture'
               path='/topics'
             />
             <CardItem
-              src='images/img_2011.png'
+              src='images/indgeo_min.png'
               text='Indian Geog'
+              path='/topics'
+            />
+            <CardItem
+              src='images/geo_min.png'
+              text='World Geog'
+              path='/topics'
+            />
+            </ul>
+            
+            <ul className='cards__items'>
+            <CardItem
+              src='images/maps_min.png'
+              text='Mapping'
+              path='/topics'
+            />
+            <CardItem
+              src='images/curr_min.png'
+              text='Current'
+              path='/topics'
+            />
+            <CardItem
+              src='images/report_min.jpeg'
+              text='Reports'
+              path='/topics'
+            />
+            </ul>
+            
+            <ul className='cards__items'>
+            <CardItem
+              src='images/schemes_min.png'
+              text='Schemes'
+              path='/topics'
+            />
+            <CardItem
+              src='images/postin_min.png'
+              text='Post Indep'
+              path='/topics'
+            />
+            <CardItem
+              src='images/misc_min.png'
+              text='Miscellaneous'
               path='/topics'
             />
             </ul>
