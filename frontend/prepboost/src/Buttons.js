18fed44ae10fc1a0e9ff212eefcec869d2a3ae98
Anudeep Reddy <bandaanudeep36@gmail.com>
2022-02-03 00:00:59 -0500

    Adding button

---
+++ frontend/prepboost/src/Buttons.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+function clickme(){
+    alert('You clicked me!');
+}
+
+function Buttons() {
+    return (
+      
+      <a href="https://www.youtube.com" target="_blank">
+          <button> Button </button>
+      </a>
+    );
+  }
+  
+  export default Buttons;
