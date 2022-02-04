8763217b2b0b3b29931bc8a4df6dc02ca514adf9
Anudeep Reddy <bandaanudeep36@gmail.com>
2022-02-04 10:40:10 -0500

    Added navigation to years and topics

---
+++ frontend/prepboost/src/components/pages/TopicQuestions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import '../../App.css';
+
+function TopicQuestions() {
+  return (
+    <>
+      
+      <h1 className='home'>Questions will be added here soon.</h1>
+    </>
+  );
+}
+
+export default TopicQuestions;
