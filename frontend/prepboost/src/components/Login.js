2a6e2333ed624c475e799f249fae976eb535b39c
srisai1234 <srisai.ananthula@gmail.com>
2022-03-04 19:53:39 -0500

    feat : Integrated the frontend with backend

--- frontend/prepboost/src/components/Login.js
+++ frontend/prepboost/src/components/Login.js
@@ -1,46 +1,94 @@
-import React from 'react'
-import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { useState } from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import axios from "axios";
 
-const Login=()=>{
+const Login = () => {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("")
 
-    const paperStyle={padding :20,height:'70vh',width:300, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
-    return(
-        <Grid className='login'>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                    <h2>Sign In</h2>
-                </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
-                <FormControlLabel
-                    control={
-                    <Checkbox
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label="Remember me"
-                 />
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography >
-                     <Link href="#" >
-                        Forgot password ?
-                </Link>
-                </Typography>
-                <Typography > Do you have an account ?
-                     <Link href="signup" >
-                        Sign Up 
-                </Link>
-                </Typography>
-            </Paper>
+    const loginUser = () => {
+    axios
+      .post("http://eaa0-2600-8807-c0c4-300-f9c0-f9c4-b44-dda3.ngrok.io/users/login/", {
+        Username: username,
+        Password: password,
+      }, {
+        "Access-Control-Allow-Origin": "*"
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 300,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+  return (
+    <Grid className="login">
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Sign In</h2>
         </Grid>
-    )
-}
+        <TextField
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+          onChange={(e) => {setUserName(e.target.value)}}
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+          onChange={(e) => {setPassword(e.target.value)}}
+        />
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+          onClick={loginUser}
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          {" "}
+          Do you have an account ?<Link href="signup">Sign Up</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Login
+export default Login;
