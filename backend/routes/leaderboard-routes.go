85c99be65a50a7414ec8f72e86071457696a2892
Bhargav Bvs <bhargavbvs2010@gmail.com>
2022-03-29 22:44:30 -0400

    feat : Add leaderboard endpoints

---
+++ backend/routes/leaderboard-routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	controllers "prepboost.com/web/controllers/leaderboard"
+)
+
+var RegisterLeaderboardRoutes = func(router *mux.Router) {
+	router.HandleFunc("/leaderboard/alltime/", controllers.LeaderboardAlltime).Methods("POST")
+	router.HandleFunc("/leaderboard/daily/", controllers.LeaderboardDaily).Methods("POST")
+}
