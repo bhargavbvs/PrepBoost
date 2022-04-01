78788eb063a185068b79d92f68c6def707fdfae2
srisai1234 <srisai.ananthula@gmail.com>
2022-03-31 21:18:31 -0400

    feat: Added contacts page to the navigation bar

---
+++ frontend/prepboost/src/components/pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import '../../App.css';
+
+class  Contact extends  React.Component{
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    return (
+      <div>    
+        <h1 className='contact'>To contact for any issues . <br/> 352XXXXX99</h1>
+      </div>
+    );
+  }
+}
+
+export default Contact;
