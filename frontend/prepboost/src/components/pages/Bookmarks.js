107ab234ac31f0bf3f0a8701299ebade9eaef1ce
Anudeep Reddy <bandaanudeep36@gmail.com>
2022-04-15 23:23:29 -0400

    feat: Integrated bookmarks page with backend

--- frontend/prepboost/src/components/pages/Bookmarks.js
+++ frontend/prepboost/src/components/pages/Bookmarks.js
@@ -1,17 +1,104 @@
-import React from 'react';
-// import '../../App.css';
+import React, { useState } from 'react';
 
-class  Bookmarks extends  React.Component{
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    return (
-      <div>    
-        <h1 className='bookmarks'>Your bookmarked questions will be visible here</h1>
-      </div>
-    );
-  }
+export default function TopicQuestions(props) {
+
+	const username = props.data
+
+	async function Login()
+	{
+		let item ={username}
+		console.warn(item)
+
+		let result = await fetch("http://711d-2600-8807-c0c4-300-e1dc-68bf-67dc-b13.ngrok.io/questions/bookmarks/",{
+			method: 'POST',
+			body:JSON.stringify(item),
+			headers:{
+				"Content-Type": 'application/json',
+				"Accept": 'application/json'
+			}
+		})
+		result = await result.json()
+		console.warn("result", result)
+	}
+
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='topicquestions'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText} </div>
+					
+					<br/>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default Bookmarks;
