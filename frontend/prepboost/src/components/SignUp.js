cdde90da4e823ff523221a60b30619d707e62478
Anudeep Reddy <bandaanudeep36@gmail.com>
2022-04-19 22:46:47 -0400

    feat: Updated code to verify and store user credentials

--- frontend/prepboost/src/components/SignUp.js
+++ frontend/prepboost/src/components/SignUp.js
@@ -9,13 +9,11 @@ import {
     Link,
   } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+import Welcome from '../components/pages/Welcome';
 
 const Signup = () => {
     const paperStyle = { padding: '30px 20px', width: 320, margin: "20px auto" }
     const btnstyle = { margin: "8px 0" };
-    const headerStyle = { margin: 0 }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const [Username,setName] = useState("")
     const [Email,setEmail] = useState("")
@@ -23,7 +21,7 @@ const Signup = () => {
     const [Password,setPassword] = useState("") 
     const Paid = 0
     const Search_left = 1
-    const Session_id = "akdhdfajdddfddsdfsddddsdffdfsafffk"
+    const Session_id = (Math.random() + 1).toString(36).substring(20);
 
     async function signUp()
     {
@@ -39,7 +37,32 @@ const Signup = () => {
             }
         })
         result = await result.json()
-        console.warn("result", result)
+        // let result = {
+        //     "ID": 10,
+        //     "Username": "bhariojiojw",
+        //     "Mobile": "+35278900990",
+        //     "Email": "bvshbsdfds@gmailcom",
+        //     "Password": "mypasios",
+        //     "Paid": 0,
+        //     "Search_left": 1,
+        //     "Session_id": "akdhdfadsdfsddddsdffdfsafffk",
+        //     "Created_at": "2022-04-19T21:42:52.532898-04:00",
+        //     "Updated_at": "2022-04-19T21:42:52.532898-04:00",
+        //     "Token": ""
+        // }
+        const values = []
+        Object.keys(result).forEach(key => values.push({name: key, value: result[key]}))
+        console.warn("values", values[1])
+
+        let id = values[0].value
+        let username = values[1].value
+        
+        return (
+            <div>
+            <Welcome id={id} username={username}/>
+            </div>
+        );
+        
     }
 
     return (
